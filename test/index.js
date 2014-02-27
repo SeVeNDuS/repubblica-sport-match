99aab7cdf46bca1c1c29e5fb4422febe659e8295
SeVeNDuS <sevendus@gmail.com>
2014-02-27 22:06:07 +0100

    Check that no error returned when url is passed

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ var RepubblicaSportMatchCrawler = require('../lib/index.js');
 describe('Repubblica Sport Match', function () {
     'use strict';
 
-    it('return err when no URL is passed', function (done) {
+    it('returns err when no URL is passed', function (done) {
         var crawler = new RepubblicaSportMatchCrawler();
         crawler.load(function (err, data) {
             should.exist(err);
@@ -14,4 +14,16 @@ describe('Repubblica Sport Match', function () {
         });
     });
 
+    it('returns no err when URL is passed', function (done) {
+        var options = {
+            url: 'http://sport.repubblica.it/tabellino/A/Bologna/Roma?refresh_ce'
+        };
+
+        var crawler = new RepubblicaSportMatchCrawler(options);
+        crawler.load(function (err, data) {
+            should.not.exist(err);
+            should.not.exist(data);
+            done();
+        });
+    });
 });
