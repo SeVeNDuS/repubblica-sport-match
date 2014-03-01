a9cd92decafd0fde4137fc73388f52d88fe3bd99
SeVeNDuS <sevendus@gmail.com>
2014-03-01 14:37:48 +0100

    Got all players scores

--- test/index.js
+++ test/index.js
@@ -1,4 +1,5 @@
 var should = require('should');
+var sinon = require('sinon');
 var RepubblicaSportMatchCrawler = require('../lib/index.js');
 
 describe('Repubblica Sport Match', function () {
@@ -36,7 +37,28 @@ describe('Repubblica Sport Match', function () {
         var crawler = new RepubblicaSportMatchCrawler(options);
         crawler.load(function (err, data) {
             should.not.exist(err);
-            should.not.exist(data);
+            done();
+        });
+    });
+
+    it('returns an array with two teams data', function (done) {
+        var options = {
+            url: 'http://sport.repubblica.it/tabellino/A/Bologna/Roma?refresh_ce'
+        };
+
+        var crawler = new RepubblicaSportMatchCrawler(options);
+        crawler.load(function (err, data) {
+            should.not.exist(err);
+            should.exist(data);
+            data.length.should.be.equal(2);
+            data[0].length.should.be.equal(14);
+
+            var player = data[0][0];
+            player.should.have.property('code');
+            player.should.have.property('name');
+            player.should.have.property('points');
+            
+            data[1].length.should.be.equal(14);
             done();
         });
     });
